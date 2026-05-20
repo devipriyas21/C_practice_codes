0b3fe18952046a691afeae1eba01fd8f0ee09c0b
Devipriya S <sdevipriya1234@gmail.com>
2026-05-20 15:11:09 +0530

    A simple if condition example.c

--- if_condition.c
+++ if_condition.c
@@ -1,2 +1,9 @@
-simple contribution
-condition contribution
+#include<stdio.h>
+int main()
+{
+  int n=5;
+  if(n>4)
+    printf("NUmber is greater than 4");
+  else
+    printf("Number is less than 4");
+}
